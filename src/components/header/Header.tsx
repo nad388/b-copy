bfc48e3986feead9078de42c11dd26ecd271674d
nad388 <nad388@yandex.ru>
2023-11-10 22:39:15 +0300

    imroved header component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react'
+import { Link } from 'react-router-dom'
 import mainLogo from '../../assets/logo.svg'
 import LoginBlock from './LoginBlock'
 import Menu from './Menu'
@@ -6,9 +7,15 @@ import Menu from './Menu'
 const Header: FC = () => {
 	return (
 		<div className='p-[1rem] '>
-			<div className='flex justify-between items-center'>
-				<img src={mainLogo} alt='logo' />
-				<Menu />
+			<div className='flex items-center'>
+				<div className='flex row items-center justify-between'>
+					<Link to='/'>
+						<img src={mainLogo} alt='logo' className='pr-10' />
+					</Link>
+
+					<Menu />
+				</div>
+
 				<LoginBlock />
 			</div>
 		</div>
--- src/components/header/Menu.tsx
+++ src/components/header/Menu.tsx
@@ -1,18 +1,15 @@
 import { FC } from 'react'
 import { Link } from 'react-router-dom'
+import styles from './Header.module.scss'
 
-const menuItems = ['ХОСТИНГ', 'VPS', 'СЕРВЕРЫ', 'ДОМЕНЫ', 'CMS', 'SSL']
+const menuItems = ['хостинг', 'VPS', 'серверы', 'домены', 'CMS', 'SSL']
 
 const Menu: FC = () => {
 	return (
 		<div className='flex'>
 			<nav>
 				{menuItems.map((item, idx) => (
-					<Link
-						to='#'
-						key={idx}
-						className='mr-[10px] text-[0.95rem] font-normal  px-0 py-[3px]'
-					>
+					<Link to='#' key={idx} className={styles.menu}>
 						{item}
 					</Link>
 				))}
